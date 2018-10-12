4657b7cbedac021c6cd1ff6cfa9e7b408b6a02f6
admunidayan <35984858+admunidayan@users.noreply.github.com>
2018-10-12 13:45:23 +0700

    update

--- application/controllers/admin/Rekaptulasi.php
+++ application/controllers/admin/Rekaptulasi.php
@@ -297,112 +297,109 @@ class Rekaptulasi extends CI_Controller {
                     // 'id_agama' => $ressult->id_agama,
                     // 'id_alat_transport' => NULL,
                     // 'kewarganegaraan' => 'ID',
-                );
-                $this->Admin_m->update_data('mahasiswa','id',$mhs->id_pd_siakad,$datamhss);
-                // data end
-                $error_count++;
-                $error[] = $val[1]." ".$val[2]." Berhasil di update";
-                // echo "<pre>";print_r($datamhss);echo "<pre/>";exit();
-              }else{
-                $sukses++;
-                $url = 'http://pmb.unidayan.ac.id/index.php/kampus/get_detail_pmb_noreg/'.@filter_var($val[32], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH);
-                // $url2 = 'http://pmb.unidayan.ac.id/index.php/kampus/get_pil_jurusan/'.@$this->input->post('id_pilihan');
-                $mhsdt = $url;
-                $ressult = json_decode(file_get_contents($url));
-                // echo "<pre>";print_r($mhsdt);echo "<pre/>";exit();
-                // $ressult2 = json_decode(file_get_contents($url2));
-                $dtsms = $this->Admin_m->detail_data('sms','kode_prodi',trim(filter_var($val[13], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)));
-                // echo "<pre>";print_r($ressult);echo "<pre/>";exit();
-                // echo $ressult->id_mhs;
-                if ($ressult == TRUE) {
-                    $datamhss = array(
-                        'nm_pd'      => $ressult->nama_mhs,
-                        'id_mhs_pt'      => $ressult->noreg,
-                        'nim'      => trim(filter_var($val[1], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
-                        'jk'      => trim(filter_var($val[3], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
-                        'nik' => trim(filter_var($val[8], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
-                        'tmpt_lahir' => trim(filter_var($val[4], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
-                        'tgl_lahir' => trim(filter_var($val[5], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
-                        'nm_ayah' => trim(filter_var($val[27], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
-                        'tgl_lahir_ayah' => $ressult->tgl_lahir_ayah,
-                        'nik_ayah' => NULL,
-                        'id_jenjang_pendidikan_ayah' => $ressult->id_jenjang_pendidikan_ayah,
-                        'id_pekerjaan_ayah' => $ressult->id_pekerjaan_ayah,
-                        'id_penghasilan_ayah' => $ressult->id_penghasilan_ayah,
-                        'id_kebutuhan_khusus_ayah' => NULL,
-                        'nm_ibu_kandung' => trim(filter_var($val[7], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
-                        'tgl_lahir_ibu' => @$ressult->tgl_lahir_ibu,
-                        'nik_ibu' => NULL,
-                        'id_jenjang_pendidikan_ibu' => @$ressult->id_jenjang_pendidikan_ibu,
-                        'id_pekerjaan_ibu' => @$ressult->id_pekerjaan_ibu,
-                        'id_penghasilan_ibu' => @$ressult->id_penghasilan_ibu,
-                        'id_kebutuhan_khusus_ibu' => NULL,
-                        'no_hp' => @$ressult->no_hp_mhs,
-                        'email' => @$ressult->email_mhs,
-                        'a_terima_kps' => 0,
-                        'no_kps' => NULL,
-                        'npwp' => NULL,
-                        'id_wil' => '999999',
-                        'id_jns_tinggal' => NULL,
-                        'id_agama' => @$ressult->id_agama,
-                        'id_alat_transport' => NULL,
-                        'kewarganegaraan' => 'ID',
                     );
-                    $this->Admin_m->insert_data('mahasiswa',$datamhss);
-                    // 
-                    $datamhs = array(
-                        'id_pd_siakad' => $this->Admin_m->last_id_mhs($ressult->noreg)->id,
-                        'kode_sms' =>filter_var($val[13], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH),
-                        'id_sp'      => $this->Admin_m->info_pt(1)->kode_info_pt,
-                        'id_sms' => $dtsms->id_sms,
-                        'nipd' => trim(filter_var($val[1], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
-                        'tgl_masuk_sp'      => date('Y-m-d'),
-                        'tgl_keluar'      => NULL,
-                        'ket'      => NULL,
-                        'skhun' => NULL,
-                        'a_pernah_paud' => 0,
-                        'a_pernah_tk' => 0,
-                        'tgl_create' => date('Y-m-d'),
-                        'mulai_smt' => $ressult->angkatan.'1',
-                        'sks_diakui' => 0,
-                        'ipk' => 0,
-                        'id_jns_daftar' => 1,
-                        'id_jns_keluar' => NULL,
-                        'id_jalur_masuk' => 8,
-                        'id_pembiayaan' => 1,
-                    );
-                    // echo "<pre>";print_r($datamhs);echo "</pre>";exit();
-                    $this->Admin_m->insert_data('mahasiswa_pt',$datamhs);
-                    // buat akun
-                    $username = trim(filter_var($val[1], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH));
-                    $email = $ressult->email_mhs;
-                    $password = $this->Admin_m->acakangkahuruf(5);
-                    $group = array('2');
-                    $additional_data = array(
-                        'first_name' => $ressult->nama_mhs,
-                        'id_mhs' =>$this->Admin_m->last_id_mhs($ressult->noreg)->id,
-                        'last_name' => $this->Admin_m->info_pt(1)->nama_info_pt,
-                        'company' => $this->Admin_m->info_pt(1)->nama_info_pt,
-                        'phone' => '123456789',
-                        'repassword' => $password,
-                    );
-                    $this->ion_auth->register($username, $password, $email, $additional_data, $group);
-                    // buat aktifitas mahasiswa
-                    $aktvkul = array(
-                        'id_smt' => $ressult->angkatan.'1',
-                        'id_mhs_pt' =>$this->Admin_m->last_id_mhs_pt($username)->id,
-                        'id_stat_mhs' => 'A',
-                        'ips' => 0,
-                        'sks_smt' => 0,
-                        'ipk' => 0,
-                        'sks_total' => 0
-                    );
-                    $this->Admin_m->insert_data('kuliah_mahasiswa',$aktvkul);
+                    $this->Admin_m->update_data('mahasiswa','id',$mhs->id_pd_siakad,$datamhss);
+                    // data end
+                    $error_count++;
+                    $error[] = $val[1]." ".$val[2]." Berhasil di update";
+                    // echo "<pre>";print_r($datamhss);echo "<pre/>";exit();
                 }
               }
             }else{
-              $error_count++;
-              $error[] = $val[1]." ".$val[2]." Mahasiswa ini tidak terdaftar.";
+              $sukses++;
+              $url = 'http://pmb.unidayan.ac.id/index.php/kampus/get_detail_pmb_noreg/'.@filter_var($val[32], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH);
+                // $url2 = 'http://pmb.unidayan.ac.id/index.php/kampus/get_pil_jurusan/'.@$this->input->post('id_pilihan');
+              $mhsdt = $url;
+              $ressult = json_decode(file_get_contents($url));
+                // echo "<pre>";print_r($mhsdt);echo "<pre/>";exit();
+                // $ressult2 = json_decode(file_get_contents($url2));
+              $dtsms = $this->Admin_m->detail_data('sms','kode_prodi',trim(filter_var($val[13], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)));
+                // echo "<pre>";print_r($ressult);echo "<pre/>";exit();
+                // echo $ressult->id_mhs;
+              if ($ressult == TRUE) {
+                $datamhss = array(
+                    'nm_pd'      => $ressult->nama_mhs,
+                    'id_mhs_pt'      => $ressult->noreg,
+                    'nim'      => trim(filter_var($val[1], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
+                    'jk'      => trim(filter_var($val[3], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
+                    'nik' => trim(filter_var($val[8], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
+                    'tmpt_lahir' => trim(filter_var($val[4], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
+                    'tgl_lahir' => trim(filter_var($val[5], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
+                    'nm_ayah' => trim(filter_var($val[27], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
+                    'tgl_lahir_ayah' => $ressult->tgl_lahir_ayah,
+                    'nik_ayah' => NULL,
+                    'id_jenjang_pendidikan_ayah' => $ressult->id_jenjang_pendidikan_ayah,
+                    'id_pekerjaan_ayah' => $ressult->id_pekerjaan_ayah,
+                    'id_penghasilan_ayah' => $ressult->id_penghasilan_ayah,
+                    'id_kebutuhan_khusus_ayah' => NULL,
+                    'nm_ibu_kandung' => trim(filter_var($val[7], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
+                    'tgl_lahir_ibu' => @$ressult->tgl_lahir_ibu,
+                    'nik_ibu' => NULL,
+                    'id_jenjang_pendidikan_ibu' => @$ressult->id_jenjang_pendidikan_ibu,
+                    'id_pekerjaan_ibu' => @$ressult->id_pekerjaan_ibu,
+                    'id_penghasilan_ibu' => @$ressult->id_penghasilan_ibu,
+                    'id_kebutuhan_khusus_ibu' => NULL,
+                    'no_hp' => @$ressult->no_hp_mhs,
+                    'email' => @$ressult->email_mhs,
+                    'a_terima_kps' => 0,
+                    'no_kps' => NULL,
+                    'npwp' => NULL,
+                    'id_wil' => '999999',
+                    'id_jns_tinggal' => NULL,
+                    'id_agama' => @$ressult->id_agama,
+                    'id_alat_transport' => NULL,
+                    'kewarganegaraan' => 'ID',
+                );
+                $this->Admin_m->insert_data('mahasiswa',$datamhss);
+                    // 
+                $datamhs = array(
+                    'id_pd_siakad' => $this->Admin_m->last_id_mhs($ressult->noreg)->id,
+                    'kode_sms' =>filter_var($val[13], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH),
+                    'id_sp'      => $this->Admin_m->info_pt(1)->kode_info_pt,
+                    'id_sms' => $dtsms->id_sms,
+                    'nipd' => trim(filter_var($val[1], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH)),
+                    'tgl_masuk_sp'      => date('Y-m-d'),
+                    'tgl_keluar'      => NULL,
+                    'ket'      => NULL,
+                    'skhun' => NULL,
+                    'a_pernah_paud' => 0,
+                    'a_pernah_tk' => 0,
+                    'tgl_create' => date('Y-m-d'),
+                    'mulai_smt' => $ressult->angkatan.'1',
+                    'sks_diakui' => 0,
+                    'ipk' => 0,
+                    'id_jns_daftar' => 1,
+                    'id_jns_keluar' => NULL,
+                    'id_jalur_masuk' => 8,
+                    'id_pembiayaan' => 1,
+                );
+                    // echo "<pre>";print_r($datamhs);echo "</pre>";exit();
+                $this->Admin_m->insert_data('mahasiswa_pt',$datamhs);
+                    // buat akun
+                $username = trim(filter_var($val[1], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH));
+                $email = $ressult->email_mhs;
+                $password = $this->Admin_m->acakangkahuruf(5);
+                $group = array('2');
+                $additional_data = array(
+                    'first_name' => $ressult->nama_mhs,
+                    'id_mhs' =>$this->Admin_m->last_id_mhs($ressult->noreg)->id,
+                    'last_name' => $this->Admin_m->info_pt(1)->nama_info_pt,
+                    'company' => $this->Admin_m->info_pt(1)->nama_info_pt,
+                    'phone' => '123456789',
+                    'repassword' => $password,
+                );
+                $this->ion_auth->register($username, $password, $email, $additional_data, $group);
+                    // buat aktifitas mahasiswa
+                $aktvkul = array(
+                    'id_smt' => $ressult->angkatan.'1',
+                    'id_mhs_pt' =>$this->Admin_m->last_id_mhs_pt($username)->id,
+                    'id_stat_mhs' => 'A',
+                    'ips' => 0,
+                    'sks_smt' => 0,
+                    'ipk' => 0,
+                    'sks_total' => 0
+                );
+                $this->Admin_m->insert_data('kuliah_mahasiswa',$aktvkul);
             }
           }
         }
